b3836170fee866d91a9e33ee7e544b8c22d8488b
Jan T. Sott <git@idleberg.com>
2021-07-10 17:52:10 +0200

    reduce bundle size

--- src/provider.js
+++ src/provider.js
@@ -2,7 +2,7 @@ import { configSchema, getConfig } from './config';
 import { EventEmitter } from 'events';
 import { satisfyDependencies } from 'atom-satisfy-dependencies';
 import Logger from './log';
-import meta from '../package.json';
+import { name } from '../package.json';
 import which from 'which';
 
 export { configSchema as config };
@@ -78,7 +78,7 @@ export function activate() {
 
   // This package depends on build, make sure it's installed
   if (getConfig('manageDependencies') === true) {
-    satisfyDependencies(meta.name);
+    satisfyDependencies(name);
   }
 }
 
